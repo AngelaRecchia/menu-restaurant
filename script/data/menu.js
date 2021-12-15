fc5fcf5313d70a3cf61b642318cda30c08d3bf0e
AngelaRecchia <angela.recchia@outlook.com>
2021-12-15 17:54:58 +0100

    show plates on click

---
+++ script/data/menu.js
@@ -0,0 +1,37 @@
+export default [
+    {
+        "name": "Margherita",
+        "description": "Salsa di pomodoro, mozzarella di bufala campana DOP, pomodorini confit, gocce di pesto e basilico",
+        "price": "$11.00",
+        "image": "https://www.ricettapizzanapoletana.it/wp-content/uploads/2019/07/pizza-napoletana.jpg",
+        "category": "Pizza"
+    },
+    {
+        "name":"Capricciosa",
+        "description": "Salsa di pomodoro, mozzarella fiordilatte, prosciutto cotto, carciofi in olio, funghi freschi e olive leccine",
+        "price": "$12.00",
+        "image": "https://www.melarossa.it/wp-content/uploads/2021/02/pizza-capricciosa.jpg",
+        "category": "Pizza"
+    },
+    {
+        "name": "Half Baked Cookie",
+        "description": "with bitter chocolate, almond biscuit, caramelised popcorn and Madagascar vanilla ice cream",
+        "price": "$12.00",
+        "image": "https://www.portraitmagazine.com/pub/photo/thumb/43-recipe-half-baked-cookie-001_cropto_768x569.jpg",
+        "category": "Dessert"
+    },
+    {
+        "name": "Souffle",
+        "description": "Valrhona chocolate, Madagascar vanilla ice cream and caramel sauce",
+        "price": "$15.00",
+        "image": "https://bellodolceicecream.com/wp-content/uploads/2018/01/Bello-Dolce-Souffle.gif",
+        "category": "Dessert"
+    },
+    {
+        "name": "Cheesecake",
+        "description": "oven baked cheesecake with salted caramel ice cream and strawberry confit",
+        "price": "$13.00",
+        "image": "https://projectveganbaking.com/wp-content/uploads/2021/04/04.jpeg",
+        "category": "Dessert"
+    }
+]
